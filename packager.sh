cc2e87c06efabcd8202875f492985d1a8c113f8e
DavidYo <lturtsamuel@gmail.com>
2020-12-03 17:39:36 +0800

    打包前做 uglify

--- packager.sh
+++ packager.sh
@@ -1,3 +1,5 @@
+set -e
+
 sys="unkown"
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     sys="linux"
@@ -7,7 +9,8 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     sys="win32"
 fi
 
-npx tsc && npx webpack\
-&& npx electron-packager . mq-savior --overwrite --platform=$sys --arch=x64 --asar --out package/tmp --ignore configs\
-&& cp package/tmp/mq-savior-$sys-x64/* package/mq-savior-$sys-x64 -R\
-&& cp ./configs package/mq-savior-$sys-x64 -Rn
+npx tsc
+npx webpack -p
+npx electron-packager . mq-savior --overwrite --platform=$sys --arch=x64 --asar --out package/tmp --ignore configs
+cp package/tmp/mq-savior-$sys-x64/* package/mq-savior-$sys-x64 -R
+cp ./configs package/mq-savior-$sys-x64 -Rn
